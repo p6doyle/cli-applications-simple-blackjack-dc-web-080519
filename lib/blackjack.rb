46bdf8ade5086d5e4b4ca40c81e5d5c5e897e5f5
Peyton Doyle <p6doyle@gmail.com>
2019-08-03 19:46:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -38,7 +38,6 @@ def hit?(card_total)
   prompt_user
   user_input = get_user_input
   while (user_input != "h" && user_input != "s")
-    prompt_user
 
   if user_input == "h"
     card_total += deal_card
